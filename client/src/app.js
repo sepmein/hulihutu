60d476065a1ba206903ff1034ef7383813c5edc7
sepmein <sepmein@Hotmail.com>
2017-05-23 23:28:16 +0800

    added material ui support

---
+++ client/src/app.js
@@ -0,0 +1,15 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const App = () => (
+    <MuiThemeProvider>
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
